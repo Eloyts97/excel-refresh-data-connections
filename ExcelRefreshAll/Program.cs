c7e101d80a7b96e370cc4759aae04b7c6425b2e7
Eloy Torres <eloytorres39@gmail.com>
2023-02-16 18:55:21 +0100

    update

--- ExcelRefreshAll/Program.cs
+++ ExcelRefreshAll/Program.cs
@@ -15,9 +15,7 @@ namespace ExcelRefreshAll
 			if (args.Length == 0 || String.IsNullOrEmpty(args[0]))
 				throw new Exception("Missing argument specifying folder to process.");
 
-			var folderPath = args[0];
-			if(!Directory.Exists(folderPath))
-				throw new Exception("Folder does not exist.");
+			var file = args[0];
 
 			Console.WriteLine("Opening Excel...");
 			var excel = new Application
@@ -25,41 +23,35 @@ namespace ExcelRefreshAll
 				ShowWindowsInTaskbar = true
 			};
 
-			var folder = Directory.GetFiles(folderPath);
-			foreach (var file in folder)
+			Console.Write("Opening '{0}'...", file);
+			var workbook = excel.Workbooks.Open(file);
+			Console.WriteLine(" done.");
+
+			var dataConnections = workbook.Connections;
+			foreach (WorkbookConnection connection in dataConnections)
 			{
-				if (!file.EndsWith(".xlsx")) continue;
-
-				Console.Write("Opening '{0}'...", file);
-				var workbook = excel.Workbooks.Open(file);
-				Console.WriteLine(" done.");
-
-				var dataConnections = workbook.Connections;
-				foreach (WorkbookConnection connection in dataConnections)
+				Console.Write("Refreshing data connection '{0}'...", connection.Name);
+				try
 				{
-					Console.Write("Refreshing data connection '{0}'...", connection.Name);
-					try
-					{
-						var originalBackgroundQuery = connection.OLEDBConnection.BackgroundQuery;
-						connection.OLEDBConnection.BackgroundQuery = false;
-						connection.Refresh();
-						connection.OLEDBConnection.BackgroundQuery = originalBackgroundQuery;
-						Console.WriteLine(" done.");
-					}
-					catch (Exception exception)
-					{
-						Console.WriteLine(" error ({0}).", exception.Message);
-					}
+					var originalBackgroundQuery = connection.OLEDBConnection.BackgroundQuery;
+					connection.OLEDBConnection.BackgroundQuery = false;
+					connection.Refresh();
+					connection.OLEDBConnection.BackgroundQuery = originalBackgroundQuery;
+					Console.WriteLine(" done.");
 				}
-
-				Console.Write("Saving workbook...");
-				workbook.Save();
-				Console.WriteLine(" done.");
-
-				Console.Write("Closing workbook...");
-				workbook.Close();
-				Console.WriteLine(" done.");
+				catch (Exception exception)
+				{
+					Console.WriteLine(" error ({0}).", exception.Message);
+				}
 			}
+
+			Console.Write("Saving workbook...");
+			workbook.Save();
+			Console.WriteLine(" done.");
+
+			Console.Write("Closing workbook...");
+			workbook.Close();
+			Console.WriteLine(" done.");
 
 			Console.Write("Closing Excel...");
 			excel.Quit();
